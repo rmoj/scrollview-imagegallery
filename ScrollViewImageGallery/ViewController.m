14e5b3b021880761884d9935179e5f8f14a7a008
rmoj <rmoj99@gmail.com>
2016-07-11 16:52:35 -0400

    Part 1

--- ScrollViewImageGallery/ViewController.m
+++ ScrollViewImageGallery/ViewController.m
@@ -8,15 +8,39 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
 @implementation ViewController
 
+- (void) prepImageViews {
+    
+    NSArray *imageArray = @[[UIImage imageNamed:@"Lighthouse-in-Field"],[UIImage imageNamed:@"Lighthouse-night"],[UIImage imageNamed:@"Lighthouse-zoomed"]];
+    
+    CGFloat xOffset = 0;
+    
+    for (UIImage *eachImage in imageArray) {
+    
+        UIImageView *imgView = [[UIImageView alloc] initWithImage:eachImage];
+        [imgView setFrame:CGRectMake(xOffset, 0, CGRectGetWidth(imgView.frame), CGRectGetHeight(imgView.frame))];
+        [self.scrollView addSubview:imgView];
+        xOffset += CGRectGetWidth(imgView.frame);
+    }
+    
+//    NSLog(@"%f",xOffset);
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.scrollView.delegate = self;
+    self.scrollView.contentSize = CGSizeMake(2210, 200);
+    [self prepImageViews];
+    
 }
 
 - (void)didReceiveMemoryWarning {
